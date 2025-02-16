29a367a19306771a03799dc0e491887af4c0c137
porat6766 <porat850@gmail.com>
2025-02-16 10:52:33 +0200

    WhatsAppClone

---
+++ Client/src/App.tsx
@@ -0,0 +1,13 @@
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <button onClick={() => {
+        console.log('clicked')
+      }}>Click me</button>
+    </div>
+  )
+}
+
+export default App
